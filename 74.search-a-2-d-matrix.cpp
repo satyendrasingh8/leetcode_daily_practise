b414f7650a8a86cc2d1c2e47d2c06e2a4fe5e60b
satyendrasingh8 <satyendrasinghsss7398@gmai.com>
2021-11-30 08:58:20 +0530

    Search a 2D Matrix in O(log(n*m))

---
+++ 74.search-a-2-d-matrix.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=74 lang=cpp
+ *
+ * [74] Search a 2D Matrix
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       if(matrix.size()==0) return false;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int low=0,high=m*n-1;
+        while(low<=high){
+            int mid=low + (high-low)/2;
+            if(matrix[mid/n][mid%n]==target) return true;
+            else if(matrix[mid/n][mid%n]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
