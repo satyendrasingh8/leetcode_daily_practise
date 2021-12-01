ba413f0abb4c5dc019eb012e0587c0b6b6bdc293
satyendrasingh8 <satyendrasinghsss7398gmail.com>
2021-12-01 08:54:57 +0530

    pow(x,n)

---
+++ 50.pow-x-n.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=50 lang=cpp
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double myPow(double x, int n) {
+         long long nn=n;
+        double ans=1.0;
+        if(nn<0) nn=-1*nn;
+        while(nn){
+            if(nn%2){
+                 ans=ans*x;
+                nn=nn-1;
+              
+            } else{
+                 x=x*x;
+                nn=nn/2;
+            }
+        }
+        if(n<0) ans=(double)(1.0)/(double)(ans);
+        return ans;
+    }
+};
+// @lc code=end
+
