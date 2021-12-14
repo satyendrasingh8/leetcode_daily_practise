1f583ebce2d538a1ad38b5c9704def025be7153b
Satyendra singh <satyendrasinghsss7398@gmail.com>
2021-12-14 09:32:52 +0530

    0 ms

---
+++ solutions/62. Unique Paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        // Using Recursion time Complexity will be exponential
+        // Using Recursion with DP time Complexity will be O(n*m)
+        //but Using trick( nCr ) we reduce it to O(m-1) or O(n-1)
+        // So by using (nCr)
+        int N=m-1+n-1;
+        int r=m-1;
+        double res=1;
+        for(int i=1;i<=r;i++){
+            res=res*(N-r+i)/i;
+        }
+        return (int)res;
+    }
+};
