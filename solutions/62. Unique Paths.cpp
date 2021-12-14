376499b4fb3b32dac427bba78533f22690a55c98
Satyendra singh <satyendrasinghsss7398@gmail.com>
2021-12-14 09:33:14 +0530

    0 ms

--- solutions/62. Unique Paths.cpp
+++ solutions/62. Unique Paths.cpp
@@ -4,7 +4,7 @@ public:
         // Using Recursion time Complexity will be exponential
         // Using Recursion with DP time Complexity will be O(n*m)
         //but Using trick( nCr ) we reduce it to O(m-1) or O(n-1)
-        // So by using (nCr)
+        // So by using (nCr)....
         int N=m-1+n-1;
         int r=m-1;
         double res=1;
