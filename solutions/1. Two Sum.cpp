e28423b5fbd744aa6238a426e41b79848083c85e
Satyendra singh <satyendrasinghsss7398@gmail.com>
2022-01-01 22:37:06 +0530

    4 ms

---
+++ solutions/1. Two Sum.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+    vector <int> res;
+    for(int i =0; i<nums.size();i++){
+        if(mp.find(nums[i]) == mp.end())
+        mp[nums[i]]=i;
+        if(mp.find(target-nums[i]) != mp.end() && i!=mp[target-nums[i]])
+        {
+            res.push_back(i);
+              res.push_back(mp[target-nums[i]]);
+            break;
+        }
+    }
+    return res;
+  
+}};
